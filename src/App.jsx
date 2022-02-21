1f916e7e07874936cd8c578451de80b165ab36bf
dk.kokorin <dk.kokorin@jet.su>
2022-02-21 13:41:15 +0300

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,101 @@
 import { useDispatch, useSelector } from "react-redux";
+import './App.css';
+import { addCustomerAction, removeCustomerAction } from "./store/customerReducer";
 
 export default function App() {
   const dispatch = useDispatch()
-  const cash = useSelector(state => state.cash)
-  
+  const cash = useSelector(state => state.cash.cash)
+  const customers = useSelector(state => state.customers.customers)
+
   const addCash = (cash) => {
-    dispatch({type: 'ADD_CASH', payload: cash})
+    dispatch({ type: 'ADD_CASH', payload: cash })
   }
 
   const getCash = (cash) => {
-    dispatch({type: 'GET_CASH', payload: cash})
+    dispatch({ type: 'GET_CASH', payload: cash })
+  }
+
+  const addCustomer = (name) => {
+    const customer = {
+      name, 
+      id: Date.now()
+    }
+    dispatch(addCustomerAction(customer))
+  }
+
+  const removeCustomer = (customer) => {
+    dispatch(removeCustomerAction(customer.id))
   }
 
   return (
     <div>
-      <div style={{fontSize: '30px'}}>{cash}</div>
+      <div style={{ fontSize: '30px' }}>{cash}</div>
 
-      <div style={{display: 'flex'}}>
-        <button onClick={() => addCash(Number(prompt()))}>Пополнить счёт</button>
-        <button onClick={() => getCash(Number(prompt()))}>Снять со счёта</button>
+      <div style={{ display: 'flex' }}>
+
+        <button onClick={() => addCash(Number(prompt()))}>
+          <div class="svg-wrapper-1">
+            <div class="svg-wrapper">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                <path fill="none" d="M0 0h24v24H0z"></path>
+                <path fill="currentColor" d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"></path>
+              </svg>
+            </div>
+          </div>
+          <span>Пополнить счёт</span>
+        </button>
+  
+        <button onClick={() => getCash(Number(prompt()))}>
+          <div class="svg-wrapper-1">
+            <div class="svg-wrapper">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                <path fill="none" d="M0 0h24v24H0z"></path>
+                <path fill="currentColor" d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"></path>
+              </svg>
+            </div>
+          </div>
+          <span>Снять со счёта</span>
+        </button>
+
+        <button onClick={() => addCustomer(prompt())}>
+          <div class="svg-wrapper-1">
+            <div class="svg-wrapper">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                <path fill="none" d="M0 0h24v24H0z"></path>
+                <path fill="currentColor" d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"></path>
+              </svg>
+            </div>
+          </div>
+          <span>Добавить клиента</span>
+        </button>
+
+        <button onClick={() => removeCustomer(prompt())}>
+          <div class="svg-wrapper-1">
+            <div class="svg-wrapper">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                <path fill="none" d="M0 0h24v24H0z"></path>
+                <path fill="currentColor" d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"></path>
+              </svg>
+            </div>
+          </div>
+          <span>Удалить клиента</span>
+        </button>
+        
       </div>
+      {customers.length > 0 
+        ?
+        <div>
+          {customers.map(customer =>
+            <div onClick={() => removeCustomer(customer)}>
+              {customer.name}
+            </div>
+          )}
+        </div>
+        :
+        <div on style={{ fontSize: '30px', marginTop: '20px' }}>
+          Клиенты отсутствуют!
+        </div>
+      }
     </div>
   );
 }
